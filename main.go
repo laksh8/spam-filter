5f66f3f5dee1d9927b0a20061b56de9b72a5d717
Laksh <lakshtaneja@yahoo.com>
2026-03-12 12:53:09 +0530

    Refactor addDirToBow extract AddFileToBow

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"strings"
@@ -35,18 +36,36 @@ func AddDirToBow(path string, bow Bow) error {
 		if d.IsDir() {
 			return nil
 		}
-
-		contents, err := os.ReadFile(path)
-		if err != nil {
-			return err
-		}
-
-		for _, token := range Tokenize(contents) {
-			bow[token] += 1
-		}
-
-		return nil
+		err = AddFileToBow(path, bow)
+		return err
 	})
+}
+
+func AddFileToBow(path string, bow Bow) error {
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	for _, token := range Tokenize(contents) {
+		bow[token] += 1
+	}
+	return nil
+}
+
+func Classify(bow Bow, hamBow Bow, spamBow Bow, totalCount int) (int, int) {
+	// Document probability
+	docProbableScore := 0.0
+	for token := range bow {
+		probable := float64(bow[token]) / float64(totalCount)
+		if probable == 0 {
+			continue
+		}
+		docProbableScore += math.Log(probable)
+	}
+	fmt.Printf("Doc probable score => %v", docProbableScore)
+
+	return 0, 0
 }
 
 func main() {
@@ -57,12 +76,12 @@ func main() {
 	for i := range 5 {
 		err := AddDirToBow(fmt.Sprintf("data/enron%v/ham", i+1), ham)
 		if err != nil {
-			log.Fatalf("Err walk: %v", err)
+			log.Fatalf("Err walk training data: %v", err)
 		}
 
 		err = AddDirToBow(fmt.Sprintf("data/enron%v/spam", i+1), spam)
 		if err != nil {
-			log.Fatalf("Err walk: %v", err)
+			log.Fatalf("Err walk training data: %v", err)
 		}
 	}
 
